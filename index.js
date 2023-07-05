d77da9bb17aa74dfd9589972b749def0b8a5bcca
andradeju <julliana.andrade08@gmail.com>
2023-07-05 12:51:34 -0300

    :art: New features e estilizações

---
+++ index.js
@@ -0,0 +1,29 @@
+let slideIndex = 0;
+const slides = document.getElementsByClassName("slide");
+
+function mostrarSlide(index) {
+  if (index < 0) {
+    slideIndex = slides.length - 1;
+  } else if (index >= slides.length) {
+    slideIndex = 0;
+  }
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[slideIndex].style.display = "block";
+}
+
+function prevSlide() {
+  slideIndex--;
+  mostrarSlide(slideIndex);
+}
+
+function nextSlide() {
+  slideIndex++;
+  mostrarSlide(slideIndex);
+}
+
+mostrarSlide(slideIndex);
+
